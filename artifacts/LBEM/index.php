6553ccb89355b6908f98701d0583db27ef8d6589
kondaa <emailarunko@gmail.com>
2021-12-14 18:14:38 -0500

    LBEM source code

---
+++ artifacts/LBEM/index.php
@@ -0,0 +1,116 @@
+<?php
+session_start();
+if(isset($_SESSION['$UserName'])){
+	header('location:Admin/index.php');
+} 
+if(isset($_SESSION['$UserName_job'])){
+	header('location:JobSeeker/index.php');
+} 
+if(isset($_SESSION['$UserName_emp'])){
+	header('location:Employer/index.php');
+} 
+?>
+<?xml version="1.0"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="cs" lang="cs">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta http-equiv="content-language" content="cs" />
+    
+    
+    <title>LBEM</title>
+    <meta name="description" content="..." />
+    <meta name="keywords" content="..." />
+    
+    <link rel="index" href="./" title="Home" />
+    <link rel="stylesheet" media="screen,projection" type="text/css" href="./css/main.css" />
+    <link rel="stylesheet" media="print" type="text/css" href="./css/print.css" />
+    <link rel="stylesheet" media="aural" type="text/css" href="./css/aural.css" />
+    <style type="text/css">
+<!--
+.style1 {
+	color: #000066;
+	font-weight: bold;
+}
+.style2 {
+	font-size: medium;
+	font-weight: bold;
+}
+-->
+    </style>
+</head>
+
+<body id="Team Nidhyana">
+<!-- Main -->
+<div id="main" class="box">
+<?php 
+include "Header.php"
+?>
+<?php 
+include "menu.php"
+?>   
+<!-- Page (2 columns) -->
+    <div id="page" class="box">
+    <div id="page-in" class="box">
+
+        <div id="strip" class="box noprint">
+
+            <!-- RSS feeds -->
+            <hr class="noscreen" />
+
+            <!-- Breadcrumbs -->
+            <p id="breadcrumbs">&nbsp;</p>
+          <hr class="noscreen" />
+            
+        </div> <!-- /strip -->
+
+        <!-- Content -->
+        <div id="content">
+
+           
+            <!-- /article -->
+
+            <hr class="noscreen" />
+
+           
+            <!-- /article -->
+
+            <hr class="noscreen" />
+            
+            <!-- Article -->
+           
+            <!-- /article -->
+
+            <hr class="noscreen" />
+
+            <!-- Article -->
+            <div class="article">
+                <h2><span><a href="#">Welcome To Local Business Employment Management</a></span></h2>
+				<h3><marquee>Welcome To LBEM</marquee></h3>
+               
+
+                <p> <span class="style2">W</span>elcome to LBEM Job Portal. It is an employment management system which handles the hiring process of the people who are interested to work in local business units. It helps the local business units to advertise their hiring process and the people who are seeking jobs.</p>
+         
+              <p align="right"> <img src="design/banner-4.jpg" alt="" width="510" height="300" /></p>
+              <p class="btn-more box noprint">&nbsp;</p>
+          </div> <!-- /article -->
+
+            <hr class="noscreen" />
+            
+        </div> <!-- /content -->
+
+<?php
+include "right.php"
+?>
+
+    </div> <!-- /page-in -->
+    </div> <!-- /page -->
+
+ 
+<?php
+include "footer.php"
+?>
+</div> <!-- /main -->
+
+</body>
+</html>
